d53921c202bab551d38f41c6d9db60a3aec277df
APPLE <apple@APPLEs-MacBook-Pro.local>
2026-05-19 08:23:28 +0700

    perf:apakek

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -6,14 +6,14 @@ import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-aut
 import { getDatabase } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-database.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCzBDdHWLMkWN7bH3oJBdri6s5KRBM6EHk",
-  authDomain: "monitoring-iot-29ac6.firebaseapp.com",
-  databaseURL: "https://monitoring-iot-29ac6-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "monitoring-iot-29ac6",
-  storageBucket: "monitoring-iot-29ac6.firebasestorage.app",
-  messagingSenderId: "4724063661",
-  appId: "1:4724063661:web:0a03e34b61072f9fd97524",
-  measurementId: "G-2Y44SR9LMN"
+  apiKey: "AIzaSyDUTkkUgSc9B5w2LC1Bk22DNDvu1h_WoU8",
+  authDomain: "anakit-bb383.firebaseapp.com",
+  databaseURL: "https://anakit-bb383-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "anakit-bb383",
+  storageBucket: "anakit-bb383.firebasestorage.app",
+  messagingSenderId: "265757518523",
+  appId: "1:265757518523:web:ee1eb4b9e36d68a24b179e",
+  measurementId: "G-XH06S279CZ"
 };
 
 const app = initializeApp(firebaseConfig);
